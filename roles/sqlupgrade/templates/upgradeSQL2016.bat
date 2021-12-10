bac8798fe8a90cd73c63c3553ad30325f4c9cc0a
Beer2Brew <jpaulcox@hotmail.com>
2021-12-10 13:45:44 -0800

    deleted roles

--- roles/sqlupgrade/templates/upgradeSQL2016.bat
+++
@@ -1,1 +0,0 @@
-\\172.16.32.239\Installs\SQL_2016_Ent\setup.exe /Q /ACTION=upgrade /IACCEPTSQLSERVERLICENSETERMS /ENU /UPDATEENABLED=false /ConfigurationFile=\\172.16.32.239\Installs\SQL_2016_Ent\Upgrade.ini
